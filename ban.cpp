8262fca40a5805d61c54ad47763fce9ff0102ef2
Zerua <Ahmed.Balttak@hotmail.com>
2014-05-31 15:23:35 -0700

    TFS 0.2.8

---
+++ ban.cpp
@@ -0,0 +1,343 @@
+//////////////////////////////////////////////////////////////////////
+// OpenTibia - an opensource roleplaying game
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//////////////////////////////////////////////////////////////////////
+#include "otpch.h"
+
+#include "definitions.h"
+
+#include "ban.h"
+#include "iologindata.h"
+#include "configmanager.h"
+#include "tools.h"
+#include "database.h"
+
+extern ConfigManager g_config;
+
+Ban::Ban()
+{
+	OTSYS_THREAD_LOCKVARINIT(banLock);
+}
+
+void Ban::init()
+{
+	maxLoginTries = (uint32_t)g_config.getNumber(ConfigManager::LOGIN_TRIES);
+	retryTimeout = (uint32_t)g_config.getNumber(ConfigManager::RETRY_TIMEOUT) / 1000;
+	loginTimeout = (uint32_t)g_config.getNumber(ConfigManager::LOGIN_TIMEOUT) / 1000;
+}
+
+void Ban::addLoginAttempt(uint32_t clientip, bool isSuccess)
+{
+	if(clientip != 0)
+	{
+		OTSYS_THREAD_LOCK(banLock, "");
+
+		uint32_t currentTime = time(NULL);
+
+		IpLoginMap::iterator it = ipLoginMap.find(clientip);
+		if(it == ipLoginMap.end())
+		{
+			LoginBlock lb;
+			lb.lastLoginTime = 0;
+			lb.numberOfLogins = 0;
+
+			ipLoginMap[clientip] = lb;
+			it = ipLoginMap.find(clientip);
+		}
+
+		if(it->second.numberOfLogins >= maxLoginTries)
+			it->second.numberOfLogins = 0;
+
+		if(!isSuccess || (currentTime < it->second.lastLoginTime + retryTimeout))
+			++it->second.numberOfLogins;
+		else
+			it->second.numberOfLogins = 0;
+
+		it->second.lastLoginTime = currentTime;
+
+		OTSYS_THREAD_UNLOCK(banLock, "");
+	}
+}
+
+bool Ban::isIpDisabled(uint32_t clientip)
+{
+	if(maxLoginTries == 0)
+		return false;
+
+	if(clientip != 0)
+	{
+		OTSYS_THREAD_LOCK(banLock, "");
+
+		uint32_t currentTime = time(NULL);
+		IpLoginMap::const_iterator it = ipLoginMap.find(clientip);
+		if(it != ipLoginMap.end())
+		{
+			if((it->second.numberOfLogins >= maxLoginTries) && (currentTime < it->second.lastLoginTime + loginTimeout))
+			{
+				OTSYS_THREAD_UNLOCK(banLock, "");
+				return true;
+			}
+		}
+		OTSYS_THREAD_UNLOCK(banLock, "");
+	}
+	return false;
+}
+
+bool IOBan::isIpBanished(uint32_t clientip)
+{
+	if(clientip == 0)
+		return false;
+
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	DBResult result;
+	query << "SELECT `ip`, `mask`, `time` FROM `bans` WHERE `type` = " << BAN_IPADDRESS << ";";
+	if(db->storeQuery(query, result))
+	{
+		for(uint32_t i = 0; i < result.getNumRows(); ++i)
+		{
+			uint32_t ip = result.getDataInt("ip", i);
+			uint32_t mask = result.getDataInt("mask", i);
+			if((ip & mask) == (clientip & mask))
+			{
+				uint32_t currentTime = time(NULL);
+				uint32_t time = result.getDataInt("time", i);
+				if(time == 0 || currentTime < time)
+					return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool IOBan::isPlayerNamelocked(const std::string& name)
+{
+	uint32_t playerId;
+	std::string playerName = name;
+	if(!IOLoginData::getInstance()->getGuidByName(playerId, playerName))
+		return true;
+
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return true;
+
+	DBQuery query;
+	DBResult result;
+	query << "SELECT `type` FROM `bans` WHERE `type` = " << NAMELOCK_PLAYER << " AND `player` = " << playerId << " LIMIT 1;";
+	if(db->storeQuery(query, result))
+		return result.getNumRows() == 1;
+
+	return false;
+}
+
+bool IOBan::isAccountBanned(uint32_t account)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	DBResult result;
+	query << "SELECT `type` FROM `bans` WHERE `type` = " << BAN_ACCOUNT << " AND `account` = " << account << " AND `time` > " << time(NULL) << " LIMIT 1;";
+	if(db->storeQuery(query, result))
+		return result.getNumRows() == 1;
+
+	return false;
+}
+
+bool IOBan::getBanInformation(uint32_t account, uint32_t& bannedBy, uint32_t& banTime, int32_t& reason, int32_t& action, std::string& comment, bool& deletion)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	DBResult result;
+	query << "SELECT `banned_by`, `time`, `reason_id`, `action_id`, `comment` FROM `bans` WHERE `type` = " << BAN_ACCOUNT << " AND `account` = " << account << " AND `time` > " << time(NULL) << " LIMIT 1;";
+	if(db->storeQuery(query, result))
+	{
+		bannedBy = result.getDataInt("banned_by");
+		banTime = result.getDataInt("time");
+		reason = result.getDataInt("reason_id");
+		action = result.getDataInt("action_id");
+		comment = result.getDataString("comment");
+		deletion = false;
+		return true;
+	}
+
+	query.str("");
+	query << "SELECT `banned_by`, `time`, `reason_id`, `action_id`, `comment` FROM `bans` WHERE `type` = " << DELETE_ACCOUNT << " AND `account` = " << account << " LIMIT 1;";
+	if(db->storeQuery(query, result))
+	{
+		bannedBy = result.getDataInt("banned_by");
+		banTime = result.getDataInt("time");
+		reason = result.getDataInt("reason_id");
+		action = result.getDataInt("action_id");
+		comment = result.getDataString("comment");
+		deletion = true;
+		return true;
+	}
+	return false;
+}
+
+int32_t IOBan::getNotationsCount(uint32_t account)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return 0;
+
+	DBQuery query;
+	DBResult result;
+	query << "SELECT `type` FROM `bans` WHERE `type` = " << NOTATION_ACCOUNT << " AND `account` = " << account << ";";
+	if(db->storeQuery(query, result))
+		return result.getNumRows();
+
+	return 0;
+}
+
+void IOBan::addIpBan(uint32_t ip, uint32_t mask, uint64_t time)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return;
+
+	DBQuery query;
+	query << "INSERT INTO `bans` (`type`, `ip`, `mask`, `time`) VALUES (" << BAN_IPADDRESS << ", " << ip << ", " << mask << ", " << time << ");";
+	db->executeQuery(query);
+}
+
+void IOBan::addPlayerNamelock(uint32_t playerId, uint32_t time, uint32_t reasonId, uint32_t actionId, std::string comment, uint32_t bannedBy)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return;
+
+	DBQuery query;
+	query << "INSERT INTO `bans` (`type`, `player`, `time`, `reason_id`, `action_id`, `comment`, `banned_by`)"
+		" VALUES (" << NAMELOCK_PLAYER << ", " << playerId << ", " << time << ", " << reasonId << ", " << actionId << ", '" << db->escapeString(comment) << "', " << bannedBy << ");";
+	db->executeQuery(query);
+}
+
+void IOBan::addAccountNotation(uint32_t account, uint64_t time, uint32_t reasonId, uint32_t actionId, std::string comment, uint32_t bannedBy)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return;
+
+	DBQuery query;
+	query << "INSERT INTO `bans` (`type`, `account`, `time`, `reason_id`, `action_id`, `comment`, `banned_by`)"
+		" VALUES (" << NOTATION_ACCOUNT << ", " << account << ", " << time << ", " << reasonId << ", " << actionId << ", '" << db->escapeString(comment) << "', " << bannedBy << ");";
+	db->executeQuery(query);
+}
+
+void IOBan::addAccountDeletion(uint32_t account, uint64_t time, int32_t reasonId, int32_t actionId, std::string comment, uint32_t bannedBy)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return;
+
+	DBQuery query;
+	query << "INSERT INTO `bans` (`type`, `account`, `time`, `reason_id`, `action_id`, `comment`, `banned_by`)"
+		" VALUES (" << DELETE_ACCOUNT << ", " << account << ", " << time << ", " << reasonId << ", " << actionId << ", '" << db->escapeString(comment) << "', " << bannedBy << ");";
+	db->executeQuery(query);
+}
+
+void IOBan::addAccountBan(uint32_t account, uint64_t time, int32_t reasonId, int32_t actionId, std::string comment, uint32_t bannedBy)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return;
+
+	DBQuery query;
+	query << "INSERT INTO `bans` (`type`, `account`, `time`, `reason_id`, `action_id`, `comment`, `banned_by`)"
+		" VALUES (" << BAN_ACCOUNT << ", " << account << ", " << time << ", " << reasonId << ", " << actionId << ", '" << db->escapeString(comment) << "', " << bannedBy << ");";
+	db->executeQuery(query);
+}
+
+bool IOBan::removePlayerNamelock(uint32_t guid)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	query << "DELETE FROM `bans` WHERE `type` = " << NAMELOCK_PLAYER << " AND `player` = " << guid << " LIMIT 1;";
+	if(!db->executeQuery(query))
+		return false;
+
+	return true;
+}
+
+bool IOBan::removeAccountNotations(uint32_t account)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	query << "DELETE FROM `bans` WHERE `type` = " << NOTATION_ACCOUNT << " AND `account` = " << account << ";";
+	if(!db->executeQuery(query))
+		return false;
+
+	return true;
+}
+
+bool IOBan::removeIPBan(uint32_t ip)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	query << "DELETE FROM `bans` WHERE `type` = " << BAN_IPADDRESS << " AND `ip` = " << ip << " LIMIT 1;";
+	if(!db->executeQuery(query))
+		return false;
+
+	return true;
+}
+
+bool IOBan::removeAccountBan(uint32_t account)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	query << "UPDATE `bans` SET `time` = " << time(NULL) << " WHERE `type` = " << BAN_ACCOUNT << " AND `account` = " << account << " AND `time` > " << time(NULL) << db->getUpdateQueryLimit() << ";";
+	if(!db->executeQuery(query))
+		return false;
+
+	return true;
+}
+
+bool IOBan::removeAccountDeletion(uint32_t account)
+{
+	Database* db = Database::getInstance();
+	if(!db->connect())
+		return false;
+
+	DBQuery query;
+	query << "DELETE FROM `bans` WHERE `type` = " << DELETE_ACCOUNT << " AND `account` = " << account << " LIMIT 1;";
+	if(!db->executeQuery(query))
+		return false;
+
+	return true;
+}
---
+++ scheduler.cpp
@@ -0,0 +1,196 @@
+//////////////////////////////////////////////////////////////////////
+// OpenTibia - an opensource roleplaying game
+//////////////////////////////////////////////////////////////////////
+// Scheduler-Objects for OpenTibia
+//////////////////////////////////////////////////////////////////////
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////
+#include "otpch.h"
+
+#include "scheduler.h"
+#include <iostream>
+
+#if defined __EXCEPTION_TRACER__
+#include "exception.h"
+#endif
+
+Scheduler::Scheduler()
+{
+	OTSYS_THREAD_LOCKVARINIT(m_eventLock);
+	OTSYS_THREAD_SIGNALVARINIT(m_eventSignal);
+	m_lastEventId = 0;
+	m_threadState = STATE_TERMINATED;
+}
+
+void Scheduler::start()
+{
+	m_threadState = STATE_RUNNING;
+	OTSYS_CREATE_THREAD(Scheduler::schedulerThread, this);
+}
+
+OTSYS_THREAD_RETURN Scheduler::schedulerThread(void* p)
+{
+	Scheduler* scheduler = (Scheduler*)p;
+	#if defined __EXCEPTION_TRACER__
+	ExceptionHandler schedulerExceptionHandler;
+	schedulerExceptionHandler.InstallHandler();
+	#endif
+	srand((unsigned int)OTSYS_TIME());
+
+	while(scheduler->m_threadState != Scheduler::STATE_TERMINATED)
+	{
+		SchedulerTask* task = NULL;
+		bool runTask = false;
+		int ret;
+
+		// check if there are events waiting...
+		OTSYS_THREAD_LOCK(scheduler->m_eventLock, "eventThread()")
+
+		if(scheduler->m_eventList.empty())
+		{
+			// unlock mutex and wait for signal
+			ret = OTSYS_THREAD_WAITSIGNAL(scheduler->m_eventSignal, scheduler->m_eventLock);
+		}
+		else
+		{
+			// unlock mutex and wait for signal or timeout
+			ret = OTSYS_THREAD_WAITSIGNAL_TIMED(scheduler->m_eventSignal, scheduler->m_eventLock, scheduler->m_eventList.top()->getCycle());
+		}
+
+		// the mutex is locked again now...
+		if(ret == OTSYS_THREAD_TIMEOUT && (scheduler->m_threadState != STATE_TERMINATED))
+		{
+			// ok we had a timeout, so there has to be an event we have to execute...
+			task = scheduler->m_eventList.top();
+			scheduler->m_eventList.pop();
+
+			// check if the event was stopped
+			EventIdSet::iterator it = scheduler->m_eventIds.find(task->getEventId());
+			if(it != scheduler->m_eventIds.end())
+			{
+				// was not stopped so we should run it
+				runTask = true;
+				scheduler->m_eventIds.erase(it);
+			}
+		}
+
+		OTSYS_THREAD_UNLOCK(scheduler->m_eventLock, "eventThread()");
+
+		// add task to dispatcher
+		if(task)
+		{
+			// if it was not stopped
+			if(runTask)
+				g_dispatcher.addTask(task);
+			else
+			{
+				// was stopped, have to be deleted here
+				delete task;
+			}
+		}
+	}
+
+	#if defined __EXCEPTION_TRACER__
+	schedulerExceptionHandler.RemoveHandler();
+	#endif
+	#ifndef WIN32
+	return NULL;
+	#endif
+}
+
+uint32_t Scheduler::addEvent(SchedulerTask* task)
+{
+	bool do_signal = false;
+
+	OTSYS_THREAD_LOCK(m_eventLock, "");
+
+	if(Scheduler::m_threadState == Scheduler::STATE_RUNNING)
+	{
+		// check if the event has a valid id
+		if(task->getEventId() == 0)
+		{
+			// if not generate one
+			if(m_lastEventId >= 0xFFFFFFFF)
+				m_lastEventId = 0;
+
+			++m_lastEventId;
+			task->setEventId(m_lastEventId);
+		}
+		// insert the eventid in the list of active events
+		m_eventIds.insert(task->getEventId());
+
+		// add the event to the queue
+		m_eventList.push(task);
+
+		// if the list was empty or this event is the top in the list
+		// we have to signal it
+		do_signal = (task == m_eventList.top());
+	}
+#ifdef __DEBUG_SCHEDULER__
+	else
+		std::cout << "Error: [Scheduler::addTask] Scheduler thread is terminated." << std::endl;
+#endif
+
+	OTSYS_THREAD_UNLOCK(m_eventLock, "");
+
+	if(do_signal)
+		OTSYS_THREAD_SIGNAL_SEND(m_eventSignal);
+
+	return task->getEventId();
+}
+
+bool Scheduler::stopEvent(uint32_t eventid)
+{
+	if(eventid == 0)
+		return false;
+
+	OTSYS_THREAD_LOCK(m_eventLock, "")
+
+	// search the event id..
+	EventIdSet::iterator it = m_eventIds.find(eventid);
+	if(it != m_eventIds.end())
+	{
+		// if it is found erase from the list
+		m_eventIds.erase(it);
+		OTSYS_THREAD_UNLOCK(m_eventLock, "");
+		return true;
+	}
+	else
+	{
+		// this eventid is not valid
+		OTSYS_THREAD_UNLOCK(m_eventLock, "");
+		return false;
+	}
+}
+
+void Scheduler::stop()
+{
+	OTSYS_THREAD_LOCK(m_eventLock, "");
+	m_threadState = Scheduler::STATE_CLOSING;
+	OTSYS_THREAD_UNLOCK(m_eventLock, "");
+}
+
+void Scheduler::shutdown()
+{
+	OTSYS_THREAD_LOCK(m_eventLock, "");
+	m_threadState = Scheduler::STATE_TERMINATED;
+
+	//this list should already be empty
+	while(!m_eventList.empty())
+		m_eventList.pop();
+
+	m_eventIds.clear();
+	OTSYS_THREAD_UNLOCK(m_eventLock, "");
+}
